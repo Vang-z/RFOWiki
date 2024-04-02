f6a2ac5893bef460bfb1214a7c44ae5ae897b2b4
Vang-z <vang-z@foxmail.com>
2024-04-02 10:02:42 +0800

    【U】2024-04-02 更新

--- src/.vuepress/components/Lead.vue
+++ src/.vuepress/components/Lead.vue
@@ -5,13 +5,13 @@ import {useRouter} from "vue-router";
 
 const Lead = () => {
   const date = `2024-04`;
-  const update_uri = `a3f1e4a3-0e5f-4855-a6ef-5ee8e6875298`;
+  const update_uri = `bc12e67d-3141-4747-aa88-8e37a9f998e4`;
   const params = new URLSearchParams(window.location.search);
   const version = params.get(`version`);
   if (!version) return;
   if (window.location.pathname.includes(update_uri)) {
     const ul = document.querySelector(`#下载链接 + ul`);
-    if (version !== `Ver@0.1.15`) {
+    if (version !== `Ver@0.1.16`) {
       const li = ul.children[0] as HTMLElement;
       li.style.textDecoration = `line-through`;
     } else {
