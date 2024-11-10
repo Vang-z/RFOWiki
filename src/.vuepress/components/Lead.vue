41ccdc00f3177796264fb8f7e757a97fa6938808
Vang-z <vang-z@foxmail.com>
2024-11-10 12:15:14 +0800

    【U】2024-11-10 更新

--- src/.vuepress/components/Lead.vue
+++ src/.vuepress/components/Lead.vue
@@ -4,20 +4,20 @@ import {useRouter} from "vue-router";
 
 
 const Lead = () => {
-  const date = `2024-10`;
-  const update_uri = `2ba484ec-446e-4bdd-baf1-0b9f1894ac19`;
+  const date = `2024-11`;
+  const update_uri = `09452ab4-4d1c-4cad-8564-9c79037ea8f2`;
   const params = new URLSearchParams(window.location.search);
   const version = params.get(`version`);
   if (!version) return;
   if (window.location.pathname.includes(update_uri)) {
-    const ul = document.querySelector(`#下载链接 + ul`);
-    if (version !== `Ver@0.1.51`) {
-      const li = ul.children[0] as HTMLElement;
-      li.style.textDecoration = `line-through`;
-    } else {
-      const li = ul.children[1] as HTMLElement;
-      li.style.textDecoration = `line-through`;
-    }
+    // const ul = document.querySelector(`#下载链接 + ul`);
+    // if (version !== `Ver@0.1.51`) {
+    //   const li = ul.children[0] as HTMLElement;
+    //   li.style.textDecoration = `line-through`;
+    // } else {
+    //   const li = ul.children[1] as HTMLElement;
+    //   li.style.textDecoration = `line-through`;
+    // }
   } else {
     window.location.href = `https://rfo.wiki/notice/${date}/${update_uri}.html?version=${version}`
   }
